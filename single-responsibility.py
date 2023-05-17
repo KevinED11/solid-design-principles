d84235242cfc7e17f8480f36b96a9aae8f943db4
KevinED11 <mendez_kael@hotmail.com>
2023-05-16 23:49:32 -0700

    2

--- single-responsibility.py
+++ single-responsibility.py
@@ -1,26 +1,33 @@
 # Solid design priciples
 from abc import ABC, abstractmethod
-
+from enum import StrEnum
 class InvalidPaymentProcessor(Exception):
-    pass
+    def __init__(self, message: str):
+        super().__init__(message)
+
+
+class PaymentProcessorType(StrEnum):
+    CREDIT = "credit"
+    DEBIT = "debit"
+    PAYPAL = "paypal"
 
 
 class ValidatePayment:
-    def validate(self, payments: list[dict[str, str]]):
+    @staticmethod
+    def validate(self, payments: list[PaymentProcessorType]):
         pass
 
 
-class AvailablePayments:
-    def available_payments(self) -> list[str]:
-        return ["credit", "debit"]
+class AvailablePaymentProccesor:
+    def get(self) -> list[PaymentProcessorType]:
+        return [payment_processor for payment_processor in PaymentProcessorType]
     
 
-class ChoicePayment:
-    def choice(self, available_payments: list[str]):
+class ChoicePaymentProcessor:
+    def choice(self, availables_payment_processor: list[PaymentProcessorType]) -> PaymentProcessorType:
         pass
 
        
-
 class AnyOrder(ABC):
     @abstractmethod
     def add_item(self, name: str, quantity: int, price: float) -> None:
@@ -47,8 +54,37 @@ class Calculator(ABC):
 
 class PaymentProcessor(ABC):
     @abstractmethod
-    def pay(self, order: AnyOrder, security_code: str) -> None:
+    def pay(self, order: AnyOrder) -> None:
         pass
+
+
+class Authorizer(ABC):
+    @abstractmethod
+    def is_authorized(self) -> bool:
+        pass
+
+
+class AuthorizerSms(Authorizer):
+    def __init__(self):
+        self.authorized = False
+
+    def verify_code(self, code: str):
+        print(f"verifying SMS code {code}")
+
+    def is_authorized(self) -> bool:
+        return self.authorized
+
+
+class AuthorizerGoogle(Authorizer):
+    def __init__(self):
+        self.authorized = False
+
+    def verify_code(self, code: str):
+        print(f"verifying google auth code {code}")
+        self.authorized = True
+
+    def is_authorized(self) -> bool:
+        return self.authorized
 
 
 class Order(AnyOrder): 
@@ -108,6 +144,15 @@ class PaypalPaymentProcessor(PaymentProcessor):
 #         print(f"Verifying security code: {security_code}")
 #         order.set_status("paid")
 
+class PaymentProcessorFactory:
+    @staticmethod
+    def create(payment_processor_type: PaymentProcessorType) -> PaymentProcessor:
+        if payment_processor_type == PaymentProcessorType.CREDIT:
+            return CreditPaymentProcessor()
+        elif payment_processor_type == PaymentProcessorType.DEBIT:
+            return DebitPaymentProcessor()
+        else:
+            raise ValueError("Tipo de procesador de pagos no soportado")
 
 class App:
     def __init__(self) -> None:
@@ -115,9 +160,16 @@ class App:
         self.price_calculator = PriceCalculator()
         self.payment_processor = self.get_payment_processor()
 
+
     def get_payment_processor(self) -> PaymentProcessor:
-        user_choice = input("Choice a option ('credit', 'debit'): ")
-        available_payments = 
+        while True:
+            user_choice = input("Ingrese el método de pago (credito/debito): ").lower()
+            if user_choice == PaymentProcessorType.CREDIT.value:
+                return PaymentProcessorFactory.create(PaymentProcessorType.CREDIT)
+            elif user_choice == PaymentProcessorType.DEBIT.value:
+                return PaymentProcessorFactory.create(PaymentProcessorType.DEBIT)
+            else:
+                print("Opción inválida. Por favor, elija 'credito' o 'debito'.")
 
 
 
@@ -130,22 +182,8 @@ class App:
 
         self.payment_processor.pay(self.order, "123")
 
-    @staticmethod
-    def main():
-        order = Order()
-        order.add_item("compuradora", 1, 500)
-        order.add_item("ssd", 1, 150)
-        
-        total_price = PriceCalculator.total_price(order)
-        print(total_price)
-
-        proccesor = CreditPaymentProcessor()
-        proccesor2 = DebitPaymentProcessor()
-        proccesor.pay(order, "123")
 
 
 if __name__ == "__main__":
-    App.main()
-
     app = App()
     app.run()
